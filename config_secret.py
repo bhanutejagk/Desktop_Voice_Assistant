23adf61d27b3814397bbeb1db481635379df888d
Bhanu Teja Goshikonda <49399204+bhanutejagk@users.noreply.github.com>
2021-06-20 11:35:13 +0530

    Add files via upload
    Extra File

---
+++ config_secret.py
@@ -0,0 +1,15 @@
+class Config:
+    # For Sending Email
+    EMAIL_PASSWORD = "guddugoogle"
+
+    # For using Tweepy API
+    CONSUMER_KEY = "cAvjXhfT51F8taSqsU89lnwCR"
+    CONSUMER_SECRET = "fJtZHKBDp51BeagHguXVJZQgvzu9frrW924BZWnvx0cM81Hx7x"
+    ACCESS_TOKEN = "1059140887287087104-2x9AGMTPEHh9XKKBk6ARS6Ns7YHO2P"
+    ACCESS_TOKEN_SECRET = "vL4lCmcv7cOxst7pTtgYBuwqdascJAJ9LqdBEZ1sGUW6d"
+
+    # For posting image to Instagram 
+    INSTA_PASSWORD = "Rr12345@"
+
+    # Path for Tessaract
+    PATH_TO_TESSERACT = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
